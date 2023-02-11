b1c186b2aa6c7723e582abfd78f98efdf7f17228
Kirill <azee12062006@gmail.com>
2023-02-12 01:14:40 +0300

    Now you can move map, the structure of the code has also been slightly changed.

--- mapapi_PG.py
+++ mapapi_PG.py
@@ -4,6 +4,11 @@ import sys
 import os
 
 
+MAP_MOVING_SPEED = 0.5
+SPN_CHANGING_SPEED = 0.062
+MAX_SPN = 5
+
+
 def get_map(ll_spn=None, map_type="map", add_params=None):
     if ll_spn:
         map_request = f"http://static-maps.yandex.ru/1.x/?{ll_spn}&l={map_type}"
@@ -42,31 +47,83 @@ def show_map(ll, spn, ll_spn=None, map_type="map", add_params=None):
                 run = False
 
         keys = pygame.key.get_pressed()
+        # Изменение масштаба карты (spn)
+        # Приближение
         if keys[pygame.K_PAGEUP] or keys[pygame.K_w]:
             spn = list(map(float, spn.split(',')))
-            spn[0] += 0.062
-            spn[1] += 0.062
-            if spn[0] < 2.5 and spn[1] < 2.5:
+            spn[0] += SPN_CHANGING_SPEED
+            spn[1] += SPN_CHANGING_SPEED
+            if spn[0] < MAX_SPN and spn[1] < MAX_SPN:
                 spn = ','.join(map(str, spn))
                 ll_spn = f'll={ll}&spn={spn}'
                 map_file = get_map(ll_spn, map_type, add_params)
             else:
-                spn[0] -= 0.062
-                spn[1] -= 0.062
+                spn[0] -= SPN_CHANGING_SPEED
+                spn[1] -= SPN_CHANGING_SPEED
                 spn = ','.join(map(str, spn))
 
+        # Отдаление
         elif keys[pygame.K_PAGEDOWN] or keys[pygame.K_s]:
             spn = list(map(float, spn.split(',')))
-            spn[0] -= 0.062
-            spn[1] -= 0.062
+            spn[0] -= SPN_CHANGING_SPEED
+            spn[1] -= SPN_CHANGING_SPEED
             if spn[0] > 0 and spn[1] > 0:
                 spn = ','.join(map(str, spn))
                 ll_spn = f'll={ll}&spn={spn}'
                 map_file = get_map(ll_spn, map_type, add_params)
             else:
-                spn[0] += 0.062
-                spn[1] += 0.062
+                spn[0] += SPN_CHANGING_SPEED
+                spn[1] += SPN_CHANGING_SPEED
                 spn = ','.join(map(str, spn))
+
+        # Изменение положения карты (ll)
+        # Движение в северном направлении
+        if keys[pygame.K_UP]:
+            ll = list(map(float, ll.split(',')))
+            ll[1] += MAP_MOVING_SPEED
+            if ll[1] < 80:
+                ll = ','.join(map(str, ll))
+                ll_spn = f'll={ll}&spn={spn}'
+                map_file = get_map(ll_spn, map_type, add_params)
+            else:
+                ll[1] -= MAP_MOVING_SPEED
+                ll = ','.join(map(str, ll))
+
+        # Движение в южном направлении
+        if keys[pygame.K_DOWN]:
+            ll = list(map(float, ll.split(',')))
+            ll[1] -= MAP_MOVING_SPEED
+            if ll[1] > -80:
+                ll = ','.join(map(str, ll))
+                ll_spn = f'll={ll}&spn={spn}'
+                map_file = get_map(ll_spn, map_type, add_params)
+            else:
+                ll[1] += MAP_MOVING_SPEED
+                ll = ','.join(map(str, ll))
+
+        # Движение в восточном направлении
+        if keys[pygame.K_RIGHT]:
+            ll = list(map(float, ll.split(',')))
+            ll[0] += MAP_MOVING_SPEED
+            if ll[0] < 179:
+                ll = ','.join(map(str, ll))
+                ll_spn = f'll={ll}&spn={spn}'
+                map_file = get_map(ll_spn, map_type, add_params)
+            else:
+                ll[0] -= MAP_MOVING_SPEED
+                ll = ','.join(map(str, ll))
+
+        # Движение в западном направлении
+        if keys[pygame.K_LEFT]:
+            ll = list(map(float, ll.split(',')))
+            ll[0] -= MAP_MOVING_SPEED
+            if ll[0] > -179:
+                ll = ','.join(map(str, ll))
+                ll_spn = f'll={ll}&spn={spn}'
+                map_file = get_map(ll_spn, map_type, add_params)
+            else:
+                ll[0] += MAP_MOVING_SPEED
+                ll = ','.join(map(str, ll))
 
         screen.blit(pygame.image.load(map_file), (0, 0))
         pygame.display.flip()
